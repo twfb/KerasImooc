0e2a78a7451c7ec09a704bcea55a8ebcc7eb4e9c
lalala <dhg___dhg@163.com>
2018-11-12 19:44:31 +0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,13 +1,15 @@
 from skimage.transform import resize
 import matplotlib.pyplot as plt
 import numpy as np
+path = 'E:\\GOD\Documents\\VsCode\\PyNoteBook\\Keras\\'
+
 
 def get_train_data(cat_or_dog, start, end):
     x = np.zeros([1, 64, 64, 3])
     y = np.zeros([1, 1])
     
     for i in range(start, end + 1):
-        image = 'E:\\GOD\Documents\\VsCode\\PyNoteBook\\Keras\\images\\train\\{}.{}.jpg'.format(cat_or_dog, i)
+        image = '{}images\\train\\{}.{}.jpg'.format(path, cat_or_dog, i)
         label = [1]
         if cat_or_dog == 'dog':
             label = [0]
@@ -24,18 +26,27 @@ def get_train_data(cat_or_dog, start, end):
 
 def save_data(cat_or_dog, train_num, test_num):
     x_train, y_train = get_train_data(cat_or_dog, 0, train_num)
-    x_test, y_test =get_train_data(cat_or_dog, 10000,10000 -1 + test_num)
-    np.savez('E:\\GOD\Documents\\VsCode\\PyNoteBook\\Keras\\{}'.format(cat_or_dog), x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test)
+    x_test, y_test = get_train_data(cat_or_dog, 10000,10000 -1 + test_num)
+    np.savez('{}{}'.format(path, cat_or_dog), x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test)
 
 
-# save_data('cat', 3000, 200)
-# save_data('dog', 3000, 200)
+# save_data('cat', 200, 20)
+# save_data('dog', 200, 20)
 
 
-def load_data(cat_or_dog):
-    data = np.load('数据集上级目录\\{}.npz'.format(cat_or_dog))
+def load_cat_or_dog_data(cat_or_dog):
+    data = np.load('{}.npz'.format(cat_or_dog))
     x_train = data['x_train']
     y_train = data['y_train']
     x_test = data['x_test']
     y_test = data['y_test']
+    return (x_train, y_train), (x_test, y_test)
+
+def load_data():
+    (x_train_cat, y_train_cat), (x_test_cat, y_test_cat) = load_cat_or_dog_data('cat')
+    (x_train_dog, y_train_dog), (x_test_dog, y_test_dog) = load_cat_or_dog_data('dog')
+    x_train = np.append(x_train_cat, x_train_dog, axis=0)
+    y_train = np.append(y_train_cat, y_train_dog, axis=0)
+    x_test = np.append(x_test_cat, x_test_dog, axis=0)
+    y_test = np.append(y_test_cat, y_test_dog, axis=0)
     return (x_train, y_train), (x_test, y_test)
